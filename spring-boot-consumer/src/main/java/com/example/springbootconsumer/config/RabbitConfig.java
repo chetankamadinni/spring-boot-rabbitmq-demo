4ab8025ae37e0f1821358bda32fe5bdd4d2ab46b
Chetan Kamadinni <chetankamadinni@gmail.com>
2020-08-20 17:50:41 +0530

    add dead letter queue feature

--- spring-boot-consumer/src/main/java/com/example/springbootconsumer/config/RabbitConfig.java
+++ spring-boot-consumer/src/main/java/com/example/springbootconsumer/config/RabbitConfig.java
@@ -1,12 +1,16 @@
 package com.example.springbootconsumer.config;
 
+import org.aopalliance.aop.Advice;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.retry.RejectAndDontRequeueRecoverer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.interceptor.RetryOperationsInterceptor;
 
 @Configuration
 @EnableRabbit
@@ -23,6 +27,15 @@ public class RabbitConfig {
 		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
 		factory.setConnectionFactory(rabbitConnectionFactory);
 		factory.setMessageConverter(jsonMessageConverter());
+		Advice[] adviceChain = new Advice[] { interceptor() };
+		factory.setAdviceChain(adviceChain);
 		return factory;
+	}
+
+	@Bean
+	RetryOperationsInterceptor interceptor() {
+		return RetryInterceptorBuilder.stateless().maxAttempts(5).backOffOptions(1000, 2.0, 10000)
+				.recoverer(new RejectAndDontRequeueRecoverer())
+				.build();
 	}
 }
--- spring-boot-consumer/src/main/java/com/example/springbootconsumer/service/ProductService.java
+++ spring-boot-consumer/src/main/java/com/example/springbootconsumer/service/ProductService.java
@@ -3,13 +3,20 @@ package com.example.springbootconsumer.service;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import com.example.springbootconsumer.exception.InvalidProductException;
 import com.example.springbootconsumer.model.Product;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
 public class ProductService {
 
 	@RabbitListener(queues = "${rabbitmq.queueName}")
-	public void getProduct(Product product) {
-		System.out.println("Product details : " + product);
+	public void getProduct(Product product) throws InvalidProductException {
+		log.info("Product details : " + product);
+		if(product.getProductId()<=0) {
+			throw new InvalidProductException();
+		}
 	}
 }
